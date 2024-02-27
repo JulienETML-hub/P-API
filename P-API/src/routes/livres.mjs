675731e0aea5ade39ad5fd96fdd39905e851b2b5
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2024-02-27 15:56:44 +0100

    gestion status http 500

--- P-API/src/routes/livres.mjs
+++ P-API/src/routes/livres.mjs
@@ -4,57 +4,64 @@ import { success, getUniqueId } from "./helper.mjs";
 const livresRouter = express();
 
 livresRouter.get("/", (req, res) => {
-  const message = "La liste des produits a bien été récupérée.";
-  res.json(success(message, livres));
+  livres.findAll()
+    .then((livres) => {
+      const message = "La liste des livres a bien été récupérée.";
+      res.json(success(message, livres));
+    })
+    .catch ((error) => {
+      const message =
+        "La liste des livres n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
+      res.status(500).json({ message, data: error });
+    });
 });
 export { livresRouter };
 
 livresRouter.get("/:id", (req, res) => {
   const livreId = req.params.id;
   const livre = livres.find((livre) => livre.id_livre == livreId);
-  const message = `Le produit dont l'id vaut ${livreId} a bien été récupéré.`;
+  const message = `Le livre dont l'id vaut ${livreId} a bien été récupéré.`;
   res.json(success(message, livre));
 });
 
 livresRouter.post("/", (req, res) => {
-  // Création d'un nouvel id du produit
+  // Création d'un nouvel id du livre
   // Dans les prochains versions, c'est MySQL qui gérera cela pour nous (identifiant auto_increment)
   const id = getUniqueId(livres);
-  // Création d'un objet avec les nouvelles informations du produits
+  // Création d'un objet avec les nouvelles informations du livres
   const createdLivre = { ...req.body, ...{ id: id, created: new Date() } };
-  // Ajout du nouveau produit dans le tableau
+  // Ajout du nouveau livre dans le tableau
   livres.push(createdLivre);
   // Définir un message pour le consommateur de l'API REST
-  const message = `Le produit ${createdLivre.name} a bien été créé !`;
-  // Retourner la réponse HTTP en json avec le msg et le produit créé
+  const message = `Le livre ${createdLivre.name} a bien été créé !`;
+  // Retourner la réponse HTTP en json avec le msg et le livre créé
   res.json(success(message, createdLivre));
 });
 
 livresRouter.delete("/:id", (req, res) => {
-    const livreId = req.params.id;
-    let deletedlivre = getLivre(livreId);
-    removeLivre(livreId);
-    // Définir un message pour le consommateur de l'API REST
-    const message = `Le produit ${deletedLivre.name} a bien été supprimé !`;
-    // Retourner la réponse HTTP en json avec le msg et le produit créé
-    res.json(success(message, deletedLivre));
-    });
+  const livreId = req.params.id;
+  let deletedlivre = getLivre(livreId);
+  removeLivre(livreId);
+  // Définir un message pour le consommateur de l'API REST
+  const message = `Le livre ${deletedLivre.name} a bien été supprimé !`;
+  // Retourner la réponse HTTP en json avec le msg et le livre créé
+  res.json(success(message, deletedLivre));
+});
 
 
-    livresRouter.put("/:id", (req, res) => {
-        const livreId = req.params.id;
-        const livre = getlivre(livreId);
-        // Mise à jour du produit
-        // A noter que la propriété 'created' n'étant pas modifiée, sera conservée telle quelle.
-        const updatedLivre = {
-        id: livreId,
-        ...req.body,
-        created: livre.created,
-        };
-        updatelivre(livreId, updatedLivre);
-        // Définir un message pour l'utilisateur de l'API REST
-        const message = `Le produit ${updatedLivre.name} dont l'id vaut ${livreId} a été mis à jour avec succès !`;
-        // Retourner la réponse HTTP en json avec le msg et le produit créé
-        res.json(success(message, updatedLivre));
-        });
-        
+livresRouter.put("/:id", (req, res) => {
+  const livreId = req.params.id;
+  const livre = getlivre(livreId);
+  // Mise à jour du livre
+  // A noter que la propriété 'created' n'étant pas modifiée, sera conservée telle quelle.
+  const updatedLivre = {
+    id: livreId,
+    ...req.body,
+    created: livre.created,
+  };
+  updatelivre(livreId, updatedLivre);
+  // Définir un message pour l'utilisateur de l'API REST
+  const message = `Le livre ${updatedLivre.name} dont l'id vaut ${livreId} a été mis à jour avec succès !`;
+  // Retourner la réponse HTTP en json avec le msg et le livre créé
+  res.json(success(message, updatedLivre));
+});
