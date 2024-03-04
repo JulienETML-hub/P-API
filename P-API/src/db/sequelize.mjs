c22a1bc1a48941c8affbcc0dc39088a7fca27bdf
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2024-03-04 10:39:17 +0100

    model utilisateurs & mockup utilisateurs & sequilize refactoriser

--- P-API/src/db/sequelize.mjs
+++ P-API/src/db/sequelize.mjs
@@ -1,37 +1,73 @@
-import { Sequelize, DataTypes} from "sequelize";
-import { livreModel } from "../models/livres.mjs";
+// Importation des modules
+import { Sequelize, DataTypes } from "sequelize";
+
+//importation des mockup
+import { utilisateurs } from "./mockup-utilisateurs.mjs";
 import { livres } from "./mockup-datas.mjs";
+
+//importation des models
+import { livreModel } from "../models/livres.mjs";
+import { utilisateursModel } from "../models/utilisateurs.mjs";
+
+
 const sequelize = new Sequelize(
-"db_livres", // Nom de la DB qui doit exister
-"root", // Nom de l'utilisateur
-"root", // Mot de passe de l'utilisateur
-{
-host: "localhost",
-port: "6033",// pour les conteneurs docker MySQL
-dialect: "mysql",
-logging: false,
-}
+  "db_livres", // Nom de la DB qui doit exister
+  "root", // Nom de l'utilisateur
+  "root", // Mot de passe de l'utilisateur
+  {
+    host: "localhost",
+    port: "6033",// pour les conteneurs docker MySQL
+    dialect: "mysql",
+    logging: false,
+  }
 );
 
+// Creation des tables a partir des modeles
 const Livre = livreModel(sequelize, DataTypes);
+const Utilisateur = utilisateursModel(sequelize, DataTypes);
+
+// Fonction pour initialiser la base de données
 let initDb = () => {
-return sequelize
-.sync({ force: true }) // Force la synchro => donc supprime les données également
-.then(() => {
-//importLivres();
-console.log("La base de données dblivres a bien été synchronisée");
-});
+  return sequelize
+    .sync({ force: true }) // Force la synchro => donc supprime les données également
+    .then(() => {
+      importLivres();
+      importUtilisateurs();
+      console.log("La base de données dblivres a bien été synchronisée");
+    });
 };
-/*
+
+// Fonction pour importer les livres
 const importLivres = () => {
   // Importe tous les livres présents dans un fichier ou une source de données
   livres.map((livre) => {
     Livre.create({
-
+      nom: livre.nom,
+      categorie: livre.categorie,
+      extrait: livre.extrait,
+      resume: livre.resume,
+      auteur: livre.auteur,
+      editeur: livre.editeur,
+      anneeEdition: livre.anneeEdition,
+      imageCouverture: livre.imageCouverture,
     }).then((livre) => console.log(livre.toJSON())).catch((error) => {
-        console.error("Erreur lors de la création du livre :", error);
-      });;
+      console.error("Erreur lors de la création du livre :", error);
+    });;
   });
-};*/
+};
+
+// Fonction pour importer les utilisateurs
+const importUtilisateurs = () => {
+  // Importe tous les utilisateurs présents dans un fichier ou une source de données
+  utilisateurs.map((utilisateur) => {
+    utilisateur.create({
+      pseudo: utilisateur.pseudo,
+      motDePasse: utilisateur.motDePasse,
+      dateEnregistrement: utilisateur.dateEnregistrement,
+    }).then((utilisateur) => console.log(utilisateur.toJSON())).catch((error) => {
+      console.error("Erreur lors de la création de l'utilisateur :", error);
+    });
+  });
+};
 
 export { sequelize, initDb, Livre };
--- P-API/src/models/livres.mjs
+++ P-API/src/models/livres.mjs
@@ -1,69 +1,68 @@
 const livreModel = (sequelize, DataTypes) => {
-    return sequelize.define(
-      "Livre",
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        titre: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        categorie: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        nombredepages: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        extrait: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
-        resume: {
-          type: DataTypes.TEXT,
-          allowNull: true,
-        },
-        auteurnom: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        auteurprenom: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        editeur: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        anneededition: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        moyennedappreciation: {
-          type: DataTypes.FLOAT,
-          allowNull: true,
-        },
-        commentaires: {
-          type: DataTypes.JSONB,
-          allowNull: true,
-          defaultValue: [],
-        },
-        imagedecouverture: {
-          type: DataTypes.STRING,
-          allowNull: true,
-        },
+  return sequelize.define(
+    "Livre",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
       },
-      {
-        timestamps: true,
-        createdAt: "created",
-        updatedAt: false,
-      }
-    );
-  };
-  
-  export { livreModel };
-  
+      titre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      categorie: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nombredepages: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      extrait: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      resume: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      auteurnom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      auteurprenom: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      editeur: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      anneededition: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      moyennedappreciation: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+      },
+      commentaires: {
+        type: DataTypes.JSONB,
+        allowNull: true,
+        defaultValue: [],
+      },
+      imagedecouverture: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: "created",
+      updatedAt: false,
+    }
+  );
+};
+
+export { livreModel };
