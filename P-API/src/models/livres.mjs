11e1f268640ca9c4b90815ee66586b925090ecea
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-03-04 10:38:48 +0100

    update model livre

--- P-API/src/models/livres.mjs
+++ P-API/src/models/livres.mjs
@@ -2,65 +2,33 @@ const livreModel = (sequelize, DataTypes) => {
     return sequelize.define(
       "Livre",
       {
-        id: {
+        id_livre: {
           type: DataTypes.INTEGER,
           primaryKey: true,
           autoIncrement: true,
         },
-        titre: {
+        nom: {
           type: DataTypes.STRING,
-          allowNull: false,
-        },
-        categorie: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        nombredepages: {
-          type: DataTypes.INTEGER,
           allowNull: false,
         },
         extrait: {
-          type: DataTypes.STRING,
-          allowNull: true,
+          type: DataTypes.INTEGER,
+          allowNull: false,
         },
         resume: {
-          type: DataTypes.TEXT,
+          type: DataTypes.STRING,
           allowNull: true,
-        },
-        auteurnom: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        auteurprenom: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
-        editeur: {
-          type: DataTypes.STRING,
-          allowNull: false,
         },
         anneededition: {
           type: DataTypes.INTEGER,
           allowNull: false,
-        },
-        moyennedappreciation: {
-          type: DataTypes.FLOAT,
-          allowNull: true,
-        },
-        commentaires: {
-          type: DataTypes.JSONB,
-          allowNull: true,
-          defaultValue: [],
-        },
-        imagedecouverture: {
-          type: DataTypes.STRING,
-          allowNull: true,
         },
       },
       {
         timestamps: true,
         createdAt: "created",
         updatedAt: false,
+        tableName:"livres"
       }
     );
   };
