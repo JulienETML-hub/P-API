173ecd60cdaf5c0cb71c205ad1fb2fe8ffa045c7
JulienETML-hub <119680240+JulienETML-hub@users.noreply.github.com>
2024-03-04 11:19:50 +0100

    mini corrections

--- P-API de mateen/src/db/mockup-livres.mjs
+++ P-API de mateen/src/db/mockup-livres.mjs
@@ -1,40 +1,40 @@
 let livres =  // Tableau de livres
 [
   {
-    "idLivre": 1,
-    "titre": "Anna Karenine",
-    "extrait": "chemin/vers/annakarenine.pdf",
-    "resume": "Le drame de ladultere dAnna Karenine, qui entraine des consequences tragiques dans la haute societe russe.",
-    "anneeEdition": 1877,
-    "imageCouverture": "chemin/vers/annakarenine.jpg",
-    "editeur": "The Russian Messenger"
+    idLivre: 1,
+    titre: "Anna Karenine",
+    extrait: "chemin/vers/annakarenine.pdf",
+    resume: "Le drame de ladultere dAnna Karenine, qui entraine des consequences tragiques dans la haute societe russe.",
+    anneeEdition: 1877,
+    imageCouverture: "chemin/vers/annakarenine.jpg",
+    editeur: "The Russian Messenger"
   },
   {
-    "idLivre": 2,
-    "titre": "Le Proces",
-    "extrait": "chemin/vers/leproces.pdf",
-    "resume": "Joseph K. est accuse dun crime mysterieux et se retrouve pris dans un systeme judiciaire kafkaïen.",
-    "anneeEdition": 1925,
-    "imageCouverture": "chemin/vers/leproces.jpg",
-    "editeur": "Verlag Die Schmiede"
+    idLivre: 2,
+    titre: "Le Proces",
+    extrait: "chemin/vers/leproces.pdf",
+    resume: "Joseph K. est accuse dun crime mysterieux et se retrouve pris dans un systeme judiciaire kafkaïen.",
+    anneeEdition: 1925,
+    imageCouverture: "chemin/vers/leproces.jpg",
+    editeur: "Verlag Die Schmiede"
   },
   {
-    "idLivre": 3,
-    "titre": "Don Quichotte",
-    "extrait": "chemin/vers/donquichotte.pdf",
-    "resume": "Les aventures de Don Quichotte et de son fidele ecuyer Sancho Panza, qui parcourent lEspagne en quete didealisme.",
-    "anneeEdition": 1605,
-    "imageCouverture": "chemin/vers/donquichotte.jpg",
-    "editeur": "Francisco de Robles"
+    idLivre: 3,
+    titre: "Don Quichotte",
+    extrait: "chemin/vers/donquichotte.pdf",
+    resume: "Les aventures de Don Quichotte et de son fidele ecuyer Sancho Panza, qui parcourent lEspagne en quete didealisme.",
+    anneeEdition: 1605,
+    imageCouverture: "chemin/vers/donquichotte.jpg",
+    editeur: "Francisco de Robles"
   },
   {
-    "idLivre": 4,
-    "titre": "Le Comte de Monte-Cristo",
-    "extrait": "chemin/vers/lecomtedemontecristo.pdf",
-    "resume": "Edmond Dantes cherche vengeance apres avoir ete trahi et emprisonne a tort.",
-    "anneeEdition": 1844,
-    "imageCouverture": "chemin/vers/lecomtedemontecristo.jpg",
-    "editeur": "Baudry"
+    idLivre: 4,
+    titre: "Le Comte de Monte-Cristo",
+    extrait: "chemin/vers/lecomtedemontecristo.pdf",
+    resume: "Edmond Dantes cherche vengeance apres avoir ete trahi et emprisonne a tort.",
+    anneeEdition: 1844,
+    imageCouverture: "chemin/vers/lecomtedemontecristo.jpg",
+    editeur: "Baudry"
   }
 ]
 
