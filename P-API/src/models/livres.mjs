53ed91f4e7d776ceaee173c3f3d3e9e42199a0e0
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2024-03-05 10:56:10 +0100

    validation du model livres

--- P-API/src/models/livres.mjs
+++ P-API/src/models/livres.mjs
@@ -10,27 +10,78 @@ const livreModel = (sequelize, DataTypes) => {
       titre: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "Le titre ne doit pas être nul",
+          },
+          notEmpty: {
+            msg: "Le titre ne doit pas être vide",
+          }
+        }
       },
       extrait: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "L'extrait ne doit pas être nul",
+          },
+          notEmpty: {
+            msg: "L'extrait ne doit pas être vide",
+          }
+        }
       },
       resume: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          notNull: {
+            msg: "Le résumé ne doit pas être nul",
+          },
+          notEmpty: {
+            msg: "Le résumé ne doit pas être vide",
+          }
+        }
       },
       anneeEdition: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        validate: {
+          notNull: {
+            msg: "L'année d'édition ne doit pas être nulle",
+          },
+          notEmpty: {
+            msg: "L'année d'édition ne doit pas être vide",
+          },
+          isInt: {
+            msg: "L'année d'édition doit être un nombre entier",
+          }
+        }
       },
       imageCouverture: {
         type: DataTypes.STRING,
         allowNull: true,
+        validate: {
+          notNull: {
+            msg: "L'image de couverture ne doit pas être nulle",
+          },
+          notEmpty: {
+            msg: "L'image de couverture ne doit pas être vide",
+          }
+        }
       },
       editeur: {
         type: DataTypes.STRING,
         allowNull: true,
-      }
+        validate: {
+          notNull: {
+            msg: "L'éditeur ne doit pas être nul",
+          },
+          notEmpty: {
+            msg: "L'éditeur ne doit pas être vide",
+          },
+        },
+      },
     },
     {
       timestamps: true,
