2293372d67443beb9ab3ed85b96996ef7c589b18
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2024-05-07 16:31:40 +0200

    ajout accueil.vue

---
+++ Code/Front-End/src/components/Accueil.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    name: 'Accueil',
+}
+</script>
+
+<template>
+    <div>
+        <h1>Accueil</h1>
+        <p>Bienvenue sur notre site de gestion de livres</p>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: #64A9CE;
+    }
+</style>
